20760a6190f960e4e597fd6cb778b0a03cf5b561
wangben <wangben-1203@qq.com>
2025-01-16 16:58:54 +0800

    field.decimal

---
+++ field/decimal.go
@@ -0,0 +1,19 @@
+package field
+
+import (
+	"database/sql/driver"
+	
+	"github.com/shopspring/decimal"
+)
+
+type Decimal struct {
+	value decimal.Decimal
+}
+
+func NewDecimal(value decimal.Decimal) *Decimal {
+	return &Decimal{value: value}
+}
+
+func (d Decimal) Value() (driver.Value, error) {
+	return d.value.InexactFloat64(), nil
+}
