f4197b373d123b688749b70d87a54daa4bc11e77
wangben <wangben-1203@qq.com>
2025-03-12 14:24:53 +0800

    logger:option

--- logger/option.go
+++ logger/option.go
@@ -1,9 +1,45 @@
 package logger
 
+import (
+	"time"
+
+	"gorm.io/gorm/logger"
+)
+
 type Option func(*Logger)
 
 func WithPath(path string) Option {
 	return func(l *Logger) {
 		l.path = path
+	}
+}
+
+func WithSlowThreshold(slowThreshold time.Duration) Option {
+	return func(l *Logger) {
+		l.slowThreshold = slowThreshold
+	}
+}
+
+func WithLogLevel(logLevel logger.LogLevel) Option {
+	return func(l *Logger) {
+		l.logLevel = logLevel
+	}
+}
+
+func WithMaxSize(maxSize int) Option {
+	return func(l *Logger) {
+		l.maxSize = maxSize
+	}
+}
+
+func WithMaxBackups(maxBackups int) Option {
+	return func(l *Logger) {
+		l.maxBackups = maxBackups
+	}
+}
+
+func WithMaxAge(maxAge int) Option {
+	return func(l *Logger) {
+		l.maxAge = maxAge
 	}
 }
