805df72cd12db9455bf5e36df8824d5e6e091e40
wangben <wangben-1203@qq.com>
2025-05-12 17:23:21 +0800

    repo

--- repository/option.go
+++ repository/option.go
@@ -8,36 +8,42 @@ func WithModule(module string) Option {
 	}
 }
 
-func WithRepoPath(repoPath string) Option {
+// WithRepositoryPath 生成仓库所在路径
+func WithRepositoryPath(repositoryPath string) Option {
 	return func(r *Repository) {
-		r.repoPath = repoPath
+		r.repoPath = repositoryPath
 	}
 }
 
+// WithGenQueryPkg gen的query.Query所在的包路径名
 func WithGenQueryPkg(genQueryPkg string) Option {
 	return func(r *Repository) {
 		r.genQueryPkg = genQueryPkg
 	}
 }
 
+// WithGormDBVar 本地存储的Gorm.DB的变量
 func WithGormDBVar(gormDBVar string) Option {
 	return func(r *Repository) {
 		r.gormDBVar = gormDBVar
 	}
 }
 
+// WithGormDBVarPkg 本地存储的Gorm.DB的变量所在的包路径名
 func WithGormDBVarPkg(gormDBVarPkg string) Option {
 	return func(r *Repository) {
 		r.gormDBVarPkg = gormDBVarPkg
 	}
 }
 
+// WithZapVar 本地存储的zap的变量
 func WithZapVar(zapVar string) Option {
 	return func(r *Repository) {
 		r.zapVar = zapVar
 	}
 }
 
+// WithZapVarPkg 本地存储的zap的变量所在的包路径名
 func WithZapVarPkg(zapVarPkg string) Option {
 	return func(r *Repository) {
 		r.zapVarPkg = zapVarPkg
